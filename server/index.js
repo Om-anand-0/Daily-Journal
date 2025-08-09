063ea6de52f15f1ba27d98773cfb39462dc25760
Om-anand-0 <om.d3v.21@gmail.com>
2025-08-10 04:37:57 +0530

    Fix CORS for production frontend

--- server/index.js
+++ server/index.js
@@ -7,7 +7,14 @@ require('dotenv').config();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    'http://localhost:5174',
+    'http://localhost:3000',
+    'https://daily-power-journal.windsurf.build'
+  ],
+  credentials: true
+}));
 app.use(express.json());
 
 // Connect to MongoDB
