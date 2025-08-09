b846869bb6d0ecbc725a9a26adc132713b91b4c2
Om-anand-0 <om.d3v.21@gmail.com>
2025-08-10 04:20:42 +0530

    Fixed bugs

--- server/index.js
+++ server/index.js
@@ -28,7 +28,7 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(clientDistPath));
 
   // ✅ Use wildcard that works in Express 5
-  app.get('*', (req, res) => {
+  app.get('/*', (req, res) => {
     res.sendFile(path.join(clientDistPath, 'index.html'));
   });
 }
