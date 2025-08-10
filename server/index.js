18492a77c74634e69f49952360c978010b23d389
Om-anand-0 <om.d3v.21@gmail.com>
2025-08-10 11:58:56 +0530

    Fix CORS for new deployment domain

--- server/index.js
+++ server/index.js
@@ -12,7 +12,8 @@ app.use(cors({
     'http://localhost:5173',
     'http://localhost:5174',
     'http://localhost:3000',
-    'https://daily-power-journal.windsurf.build'
+    'https://daily-power-journal.windsurf.build',
+    'https://daily-power-journal-redesigned.windsurf.build'
   ],
   credentials: true
 }));
